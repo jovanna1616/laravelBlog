7249ea9ccc2d82e3ea3368b71e36c864182fbeef
jovanna <ladybutterfly.fly@gmail.com>
2017-10-03 16:30:29 +0200

    auth

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -11,7 +11,7 @@ class PostsController extends Controller
 
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::latest()->get();
 
 
         // Get the currently authenticated user...
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -6,38 +6,34 @@
 
     <hr>
 
-    <form method="POST" action="/registration">
+    <form method="POST" action="/posts">
 
         {{ csrf_field() }}
 
         <div class="form-group">
             <label for="title">Title</label>
             <input type="text" class="form-control" id="title" name="title">
+            @include(
+                    'partials.error-messages',
+                    ['fieldTitle' => 'title']
+                )
         </div>
 
         <div class="form-group">
             <label for="body">Body</label>
             <textarea class="form-control" id="body" name="body"></textarea>
+            @include(
+                    'partials.error-messages',
+                    ['fieldTitle' => 'body']
+                )
         </div>
-
+        <!-- <div class="form-group">
+            <label for="published">Publish?</label>
+            <input type="checkbox" class="form-control" id="published" name="published1" value="1"/>
+        </div> -->
         <div class="form-group">
             <button type="submit" class="btn btn-primary">Submit</button>
         </div>
 
     </form>
-
-
-    @if (count($errors->all()) > 0)
-
-        @foreach($errors->all() as $error)
-            <div class="form-group">
-                <div class="alert alert-danger">
-                    <li>{{ $error }}</li>
-                </div>
-            </div>
-        @endforeach
-
-    @endif
-
-
 @endsection
